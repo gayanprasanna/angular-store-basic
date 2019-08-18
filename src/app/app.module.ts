c8390431054ab85e0af0e60bfd347b7f8f2a1dd4
Gayan Prasanna <gayan@Gayans-MacBook-Pro.local>
2019-08-18 20:08:51 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
+import { StoreModule } from "@ngrx/store";
+import { reducer } from "./reducers/tutorial.reducer";
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  declarations: [AppComponent, ReadComponent, CreateComponent],
+  imports: [BrowserModule, AppRoutingModule,
+    StoreModule.forRoot({
+      tutorial:reducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
